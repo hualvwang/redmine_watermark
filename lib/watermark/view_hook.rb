642fd2d56b10bad3bf358bce3b5ecbe4765fac89
laoshancun <laoshancun@foxmail.com>
2024-04-27 12:42:38 +0800

    chore: replace watermark code

--- lib/watermark/view_hook.rb
+++ lib/watermark/view_hook.rb
@@ -9,45 +9,19 @@ module Watermark
 
       return html if Setting.plugin_redmine_watermark['watermark_enable'] != 'on'
 
-      count = 0
-      tmp_arr = []
-      if Setting.plugin_redmine_watermark['watermark_enable_username'] == 'on'
-        count += 1
-        tmp_arr << User.current.name
-      end
-      if Setting.plugin_redmine_watermark['watermark_enable_timestamp'] == 'on'
-        count += 1
-        tmp_arr << format_date(Time.current)
-      end
-      if Setting.plugin_redmine_watermark['watermark_custom_text'].present?
-        count += 1
-        tmp_arr << Setting.plugin_redmine_watermark['watermark_custom_text']
-      end
-
-      count.times do |i|
-        html << "\n
-          <div class='watermark watermark#{i+1}'></div>
-        \n"
-        html << "
-          <script type=\"text/javascript\">\n
-            $(function(){
-              const generateWatermark = (name) => {
-                return `<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='300px' width='300px'><text transform='translate(100, 300) rotate(-30)' fill='rgba(45,45,45,0.1)' font-size='18'>${encodeURIComponent(name)}</text></svg>`;
-              };
-              var watermark#{i+1} = '#{tmp_arr[i]}';
-              const finalContent#{i+1} = decodeURIComponent(generateWatermark(watermark#{i+1}));
-              $('.watermark#{i+1}').css('background-image', `url(\"data:image/svg+xml;charset=utf-8,${finalContent#{i+1}}\")`);
-              $('.watermark#{i+1}').css('height', $('#wrapper').height() + 'px');
-            })
-          "
-        html << "\n</script>\n"
-      end
       html << "
           <script type=\"text/javascript\">\n
-          $(window).resize(function() {
-            $('.watermark').css('height', $('#wrapper').height() + 'px');
+          !function(t,e){'object'==typeof exports&&'undefined'!=typeof module?module.exports=e():'function'==typeof define&&define.amd?define(e):t.watermark=e()}(this,(function(){'use strict';return function(){function t(){}return t.create=function(t){var e=this;this.deleteWatermark(),this.config=t,this.setWatermark(this.config),this.observer=new MutationObserver((function(t){for(var r=0,a=t;r<a.length;r++){var o=a[r];o.target===e.watermark&&e.setWatermark(e.config);for(var n=0,i=Array.from(o.removedNodes);n<i.length;n++){i[n]===e.watermark&&e.setWatermark(e.config)}}})),this.observer.observe(document.body,{childList:!0,subtree:!0,attributes:!0})},t.setWatermark=function(t){this.watermark&&this.watermark.remove();var e='1.23452384164.123412416';null!==document.getElementById(e)&&document.body.removeChild(document.getElementById(e));var r=document.createElement('canvas');r.width=300,r.height=230;var a=r.getContext('2d');a.rotate(-20*Math.PI/180),a.font='14px Vedana',a.fillStyle=t.fontColorWhite?'rgba(255, 255, 255)':'rgba(200, 200, 200)',a.textBaseline='middle','dark'!=t.mode&&(a.globalAlpha=.3,a.fillText(t.content,0,60),a.fillText(t.customText||'',0,76)),a.fillStyle=t.fontColorWhite?'rgba(255, 255, 255)':'rgba(0,0,0)',a.globalAlpha=.005;var o=window.location.host,n=(new Date).toLocaleString(),i=t.secretContent||t.content;a.fillText(''.concat(i,'-').concat(i),0,92);for(var s=1;s<6;s++)a.fillText(''.concat(n,'-').concat(n),0,60+48*s),a.fillText(''.concat(i,'-').concat(i),0,60+48*s+16),a.fillText(''.concat(o,'-').concat(o),0,60+48*s+32);return this.watermark=document.createElement('div'),this.watermark.id=e,this.watermark.style.pointerEvents='none',this.watermark.style.top='15px',this.watermark.style.left='15px',this.watermark.style.position='fixed',this.watermark.style.zIndex='10000000',this.watermark.style.width='5000px',this.watermark.style.height='3000px',this.watermark.style.background='url('.concat(r.toDataURL('image/png'),') left top repeat'),document.body.appendChild(this.watermark),e},t.deleteWatermark=function(){this.observer&&this.observer.disconnect(),this.watermark&&this.watermark.remove()},t}()}));     
+          $(window).ready(function() {
+            Watermark.create({
+              content: '#{User.current.name} #{User.current.id}}',
+              secretContent: '#{User.current.id} #{format_date(Time.current)}', // 可选，暗水印内容，不填默认使用content
+              mode: 'light', // 可选，切换只使用暗水印模式
+              customText: '#{Setting.plugin_redmine_watermark['watermark_custom_text']}',
+              fontColorWhite: false, // 可选，字体颜色, 如果界面是深色主题，使用固定值true
+            });
           });
-      "
+          "
       html << "\n</script>\n"
       return html
     end
